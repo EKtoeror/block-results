8d63125e226f2034285b9060cdbb6b92086bad8d
Rushan <ektoeror777@gmail.com>
2023-01-28 21:11:10 +0300

    make code

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//string[] array = { "hello", "2", "world", ":-)" ,"3"};
+
+Console.WriteLine("Введите элемента массива через запятую");//ввод массива с клавиатуры
+string entry = Console.ReadLine() ?? "";
+string[] array = entry.Split(",");
+
+int size = 0; //определение размера нового массива
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        size++;
+    }
+}
+
+string[] newArray = new string[size]; //определение нового массива
+
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[j] = array[i];
+        j++;
+    }
+}
+Console.Write("[");
+Console.Write(string.Join(",", newArray));
+Console.Write("]");
